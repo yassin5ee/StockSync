5f8861499585df56f5893f796b399e046bd900ec
aarf101 <ayoub.arfaoui@supcom.tn>
2025-11-13 22:07:21 +0100

    Sorties Preparateur Commandes

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PreparateurCommandes from './components/preparateur-commandes/PreparateurCommandes';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <PreparateurCommandes />
+    </div>
+  );
+}
+
+export default App;
